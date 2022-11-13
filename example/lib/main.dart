3b17f2d19aa0b95b7eb94c21fa8521f8e1502007
Jeevan Joshi <joshijeevan98@gmail.com>
2022-11-13 19:01:57 +0530

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:stylish/stylish.dart';
 
-void main() {}
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Container(
+              color: Styles.c.dodgerBlue,
+            ),
+          ),
+        ),
+      );
+}
